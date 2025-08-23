4ede04f206675a5b3ce575a29bf57856004ea854
am <adrianmarino10@gmail.com>
2025-08-23 13:04:24 +0200

    part 2 done

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
 import os
 from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
 
 load_dotenv()
 
+breed = 'Border Collie'
+
+
 def main():
     print("Hello from langchain-course!!!")
+
+    template = '''
+    Please invent a cool name for a female dog which is a {breed}.
+    '''
+
+    prompt = PromptTemplate(input_variables=['breed'],
+                            template=template)
+
+    llm = ChatOpenAI(model='gpt-3.5-turbo', temperature=0.75)
+
+    chain = prompt | llm  # remember it as I want to pass the prompt -> the LLM!
+
+    res = chain.invoke(breed)
+
+    print(res.content)
+
 
 
 if __name__ == "__main__":
